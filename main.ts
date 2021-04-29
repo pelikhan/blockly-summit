a9be9e9e5020e2f4c7839ff692270207a25d1054
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-04-28 19:05:24 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ let mySprite = sprites.create(img`
     . . . . . . . . . b 5 b . . . . 
     . . . . . . . . . b c . . . . . 
     . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 5 d f . . 
-    . . . . b 5 5 1 f f 5 d 4 c . . 
-    . . . . b 5 5 d f b d d 4 4 . . 
-    b d d d b b d 5 5 5 4 4 4 4 4 b 
-    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    . . . . . 3 3 3 3 5 5 5 b . 3 . 
+    . . . . 3 b 5 d 3 3 3 3 3 3 3 . 
+    . . . 3 b 3 3 3 3 3 3 3 3 3 3 3 
+    . . . 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    b d d 3 b b 3 3 5 5 4 4 4 4 3 3 
+    b b d 3 5 3 3 5 5 4 4 4 4 4 3 3 
+    b d c 3 3 5 5 d 5 5 5 5 5 b 3 3 
     c d d c d 5 5 b 5 5 5 5 5 5 b . 
     c b d d c c b 5 5 5 5 5 5 5 b . 
     . c d d d d d d 5 5 5 5 5 d b . 
